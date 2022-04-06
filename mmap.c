066a727c50b17f7897865d7a453d612218d48340
Vrushabh <patilvp19.comp@coep.ac.in>
2022-04-07 00:19:36 +0530

    reading using readi in memory

--- mmap.c
+++ mmap.c
@@ -1,9 +1,14 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-
+#include "fcntl.h"
 int main(void) {
-	printf(1,"mmap %d",mmap(1,2,3));
+	int fd = open("mmaptest.txt", O_RDWR);
+	printf(1,"==>%d\n", fd);
+	char *addr = (char *) mmap(10,fd,2);
+	char buff[256];
+  	strcpy(buff, addr);
+  	printf(1, "%s\n", (char*)buff);
 	exit();
 }
 
